a5e727cab90b0b785183298a3e6c39e64b16f35c
Rachael Stanislaw <rachaelstanislaw@gmail.com>
2020-10-25 11:47:47 -0700

    update to map

--- src/App.js
+++ src/App.js
@@ -7,39 +7,41 @@ export default class App extends Component {
     super(props)
     this.state = {
       hex: "#000000",
-      messageArr: ["pretty!", "mmm no.", "love it!", "yuck!", "YAS!"],
       message: ""
     }
   }
 
   changeColor = () => {
+    // destructure state
     let { hex, messageArr } = this.state
-    let hexArr = hex.split("")
-    let newColor = ["#"]
-    //iterate through the length of the array.
-    for(let i = 1; i < hexArr.length; i++){
-        //randomize the index value between a integer or a letter
-        hexArr[i] = Math.floor(Math.random() * 15)
-        //if below 9, just return value of the integer
-        if(hexArr[i] <= 9) {
-            newColor.push(hexArr[i])
-            //If its greater than 9, set a letter value
-        } else if(hexArr[i] === 10){
-            newColor.push("a")
-        } else if(hexArr[i] === 11){
-            newColor.push("b")
-        } else if(hexArr[i] === 12){
-            newColor.push("c")
-        } else if(hexArr[i] === 13){
-            newColor.push("d")
-        } else if(hexArr[i] === 14){
-            newColor.push("e")
-        } else {
-            newColor.push("f")
-        }
-    }
-    let color = newColor.join("").toUpperCase()
-    let randomMessage = Math.floor(Math.random() * 5)
+    // create a new array to iterate through to create a random hex code
+    let hexArr = [...Array(7).fill("")]
+    // //iterate through the length of the hexArr using map
+    let color = hexArr.map((value, index) => {
+      //randomized number will generate a random character
+      let random = Math.floor(Math.random() * 15)
+      // first element of array will need to be "#"
+      if(index === 0) {
+        return "#"
+      // if random number is between 0 and 9, return that number
+      } else if(random <= 9) {
+          return random
+          //If it is greater than 9, set a letter value
+      } else if(random === 10){
+          return "a"
+      } else if(random === 11){
+          return "b"
+      } else if(random === 12){
+          return "c"
+      } else if(random === 13){
+          return "d"
+      } else if(random === 14){
+          return "e"
+      } else {
+          return "f"
+      }
+    }).join("").toUpperCase()
+    // set the state of the new hex code and change the message that displays to the user
     this.setState({ hex: color, message: "Click for a new color" })
   }
 
